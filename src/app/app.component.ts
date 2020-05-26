b53b8137f2577ba284bd631329c0221b80cc0ec2
gutovesco <guto.vesco@hotmail.com>
2020-05-26 12:37:39 -0300

    ifs and function of the todos buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,23 @@ export class AppComponent {
     this.todos.push(new Todo('2', 'Segunda', false));
     this.todos.push(new Todo('3', 'terceira', true));
   }
+
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo)
+    if(index !== -1){
+      this.todos.splice(index, 1)
+    }
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true
+  }
+
+  addTodo(todo: Todo){
+    this.todos.push(todo)
+  }
+
+  markAsUndone(todo: Todo){
+    todo.done = false
+  }
 }
